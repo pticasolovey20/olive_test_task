5a4d63463f2f166c8e78f85ca59d658cd34bdfba
Dmytro Solovyov <dy.solovyov.work@gmail.com>
2025-04-08 15:05:08 +0300

    create global theme customization

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,6 @@
 import type { Metadata } from 'next';
 import { ReactNode } from 'react';
-import './globals.scss';
+import '../styles/globals.scss';
 
 export const metadata: Metadata = {
 	title: 'Create Next App',
